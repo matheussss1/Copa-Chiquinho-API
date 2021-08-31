9f6bb305d3825511f2ab73d1ca5029ffafc1be84
matheussss1 <matheusfelipe_contato@hotmail.com>
2021-08-30 23:12:20 -0300

    feat: update no projeto

--- ranking/urls.py
+++ ranking/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_ranking, name="ranking"),
+    path('ranking/', views.show_ranking, name="ranking"),
+    # path(r'upload/<str:player>/<str:filename>/', views.show_image, name="player_image"),
 ]
--- ranking/views.py
+++ ranking/views.py
@@ -2,6 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import JogadorSerializer
 
+from django.http import FileResponse
+
 from .models import Jogador
 
 @api_view(['GET'])
@@ -9,3 +11,9 @@ def show_ranking(request):
     ranking = Jogador.objects.all()
     serializer = JogadorSerializer(ranking, many=True)
     return Response(serializer.data)
+
+# @api_view(['GET'])
+# def show_image(request, player, filename):
+#     path = f'upload/player_teste/download.jpg'
+#     img = open(path)
+#     return FileResponse(img)
